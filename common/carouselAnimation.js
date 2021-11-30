8951919532485919fb8b46afe5257a51de97f293
PleaseLetMe(yinhong) <yinhong@xunlei.com>
2021-11-30 17:29:29 +0800

    fix:扩大箭头的指示区域

--- common/carouselAnimation.js
+++ common/carouselAnimation.js
@@ -1,48 +1,41 @@
 /**
  * 上下轮播动画函数
  * @param {String} flag 判断此时是选年（navYear）还是选月（navMonth）还是选日（cnt）
- * @param {Object} params 
+ * @param {Object} params
  */
-import {
-  getYy,
-  getMo,
-  toolLeftOBJ,
-} from '../js/index.js'
-import {
-  showContent
-} from '../js/showContent.js'
-import {
-  showNav
-} from '../js/showNav.js'
-let bottomUp = 0
+import { getYy, getMo, toolLeftOBJ } from "../js/index.js";
+import { showContent } from "../js/showContent.js";
+import { showNav } from "../js/showNav.js";
+let bottomUp = 0;
 export function restoreBottomUp() {
-  bottomUp = 0
+  bottomUp = 0;
 }
 export function carouselAnimation(flag, params) {
   let newArr = [],
-    year = (getYy() + "").substr(0, 3) + 0 + "-" + (getYy() + "").substr(0, 3) + 9;
+    year =
+      (getYy() + "").substr(0, 3) + 0 + "-" + (getYy() + "").substr(0, 3) + 9;
   for (let i = 0; i < params.showObj.childNodes.length; i++) {
-    newArr.push(params.showObj.childNodes[i].id)
+    newArr.push(params.showObj.childNodes[i].id);
   }
-  if (flag === 'cnt') {
+  if (flag === "cnt") {
     toolLeftOBJ.innerHTML = getYy() + "年" + getMo() + "月";
-  } else if (flag === 'navMonth') {
+  } else if (flag === "navMonth") {
     toolLeftOBJ.innerHTML = getYy() + "年";
-  } else if (flag === 'navYear') {
+  } else if (flag === "navYear") {
     toolLeftOBJ.innerHTML = year;
   }
 
-  let whether = !newArr.includes(toolLeftOBJ.innerHTML)
+  let whether = !newArr.includes(toolLeftOBJ.innerHTML);
   if (whether) {
     switch (flag) {
-      case 'cnt':
-        showContent(getYy(), getMo(), params.action)
+      case "cnt":
+        showContent(getYy(), getMo(), params.action);
         break;
-      case 'navMonth':
-        showNav('月', params.action)
+      case "navMonth":
+        showNav("月", params.action);
         break;
-      case 'navYear':
-        showNav('年', params.action)
+      case "navYear":
+        showNav("年", params.action);
         break;
       default:
         break;
@@ -50,25 +43,29 @@ export function carouselAnimation(flag, params) {
   }
   // css3的写法
   // 获取当前的bottom====>带单位的
-  let bottom = getComputedStyle(params.showObj).bottom
+  let bottom = getComputedStyle(params.showObj).bottom;
   // 去单位
-  bottomUp = Number(bottom.substr(0, bottom.length - 2))
-  if (params.action === 'down' && whether) {
+  bottomUp = Number(bottom.substr(0, bottom.length - 2));
+  if (params.action === "down" && whether) {
     //说明此时的情况是  在下面添加  同时向上滚
     // 1.先退一步，再往前走
-    params.showObj.style.bottom = -(params.bottomUp) + 'px'
+    params.showObj.style.bottom = -params.bottomUp + "px";
     setTimeout(() => {
       // 2.动态给css3属性
-      params.showObj.style.transition = 'bottom 1s'
-      params.showObj.style.bottom = 0 + 'px'
+      params.showObj.style.transition = "bottom 500ms ease-in";
+      params.showObj.style.bottom = 0 + "px";
     }, 1);
-    params.showObj.addEventListener("transitionend", () => {
-      // 3.动画结束后 再把css3属性去除，（利用无transition属性时，给bottom时那一瞬间的页面变化，骗过人类的眼睛）
-      params.showObj.style.transition = ''
-    }, false)
+    params.showObj.addEventListener(
+      "transitionend",
+      () => {
+        // 3.动画结束后 再把css3属性去除，（利用无transition属性时，给bottom时那一瞬间的页面变化，骗过人类的眼睛）
+        params.showObj.style.transition = "";
+      },
+      false
+    );
   } else {
-    params.showObj.style.transition = 'bottom 1s'
-    bottomUp += params.bottomUp
-    params.showObj.style.bottom = bottomUp + 'px'
+    params.showObj.style.transition = "bottom 500ms ease-in";
+    bottomUp += params.bottomUp;
+    params.showObj.style.bottom = bottomUp + "px";
   }
 }
--- js/showTools.js
+++ js/showTools.js
@@ -1,113 +1,112 @@
 import {
-    toolLeftOBJ,
-    slideshowObj,
-    navShowObj,
-    setYy,
-    getYy,
-    setMo,
-    getMo,
-} from './index.js'
-import {
-    showNav
-} from './showNav.js'
-import {
-    removeNav
-} from '../common/removeNav.js'
-import {
-    throttle
-} from '../common/throttle.js'
-import {
-    carouselAnimation
-} from '../common/carouselAnimation.js'
+  toolLeftOBJ,
+  slideshowObj,
+  navShowObj,
+  setYy,
+  getYy,
+  setMo,
+  getMo,
+} from "./index.js";
+import { showNav } from "./showNav.js";
+import { removeNav } from "../common/removeNav.js";
+import { throttle } from "../common/throttle.js";
+import { carouselAnimation } from "../common/carouselAnimation.js";
 // 展示tools区域
 export function showTools() {
-    // 获取tools的各个对象
-    let toolRight1OBJ = document.getElementsByClassName("toolRight1")[0];
-    let toolRight2OBJ = document.getElementsByClassName("toolRight2")[0];
-    //   tools左侧初始化
-    toolLeftOBJ.innerHTML = getYy() + "年" + getMo() + "月";
-    // 每次变化年月之后，都要将变化后的年月暴露出去
+  // 获取tools的各个对象
+  let toolRight1OBJ = document.getElementsByClassName("toolRight1")[0];
+  let toolRight2OBJ = document.getElementsByClassName("toolRight2")[0];
+  //   tools左侧初始化
+  toolLeftOBJ.innerHTML = getYy() + "年" + getMo() + "月";
+  // 每次变化年月之后，都要将变化后的年月暴露出去
 
-    //   tools左侧点击事件
-    toolLeftOBJ.onclick = function () {
-        let str = toolLeftOBJ.innerHTML;
-        if (str[str.length - 1] === "月") { // 说明这里是选月的导航
-            toolLeftOBJ.innerHTML = getYy() + "年";
-            document.getElementsByClassName("slideHeight")[0].style.display = "none"
-            removeNav()
-            showNav('月');
-        } else if (str[str.length - 1] === "年") { // 说明这里是选年的导航
-            toolLeftOBJ.innerHTML =
-                (getYy() + "").substr(0, 3) + 0 + "-" + (getYy() + "").substr(0, 3) + 9;
-            removeNav()
-            showNav('年');
-        }
-    };
+  //   tools左侧点击事件
+  toolLeftOBJ.onclick = function () {
+    let str = toolLeftOBJ.innerHTML;
+    if (str[str.length - 1] === "月") {
+      // 说明这里是选月的导航
+      toolLeftOBJ.innerHTML = getYy() + "年";
+      document.getElementsByClassName("slideHeight")[0].style.display = "none";
+      removeNav();
+      showNav("月");
+    } else if (str[str.length - 1] === "年") {
+      // 说明这里是选年的导航
+      toolLeftOBJ.innerHTML =
+        (getYy() + "").substr(0, 3) + 0 + "-" + (getYy() + "").substr(0, 3) + 9;
+      removeNav();
+      showNav("年");
+    }
+  };
 
-    // 绑定点击事件
-    toolRight1OBJ.onclick = function () {
-        if (!throttle(1000)) return;
-        let str = toolLeftOBJ.innerHTML;
-        // 这里我要判断当前是选日还是选月还是选年
-        if (str[str.length - 1] === "月") { // 说明这里是选日的
-            if (getMo() > 1) {
-                setMo(getMo() - 1)
-            } else {
-                setMo(12)
-                setYy(getYy() - 1)
-            }
-            carouselAnimation('cnt', {
-                'action': 'up',
-                'bottomUp': -276,
-                'showObj': slideshowObj,
-            })
-        } else if (str[str.length - 1] === "年") { // 说明这里是选月的
-            setYy(getYy() - 1);
-            carouselAnimation('navMonth', {
-                'action': 'up',
-                'bottomUp': -304,
-                'showObj': navShowObj,
-            })
-        } else { // 说明这里是选年的
-            setYy(getYy() - 10);
-            carouselAnimation('navYear', {
-                'action': 'up',
-                'bottomUp': -304,
-                'showObj': navShowObj,
-            })
-        }
-    };
-    toolRight2OBJ.onclick = function () {
-        if (!throttle(1000)) return;
-        let str = toolLeftOBJ.innerHTML;
-        // 这里我要判断当前是选日还是选月还是选年
-        if (str[str.length - 1] === "月") { // 说明这里是选日的
-            if (getMo() < 12) {
-                setMo(getMo() + 1);
-            } else {
-                setMo(1);
-                setYy(getYy() + 1);
-            }
-            carouselAnimation('cnt', {
-                'action': 'down',
-                'bottomUp': 276,
-                'showObj': slideshowObj,
-            })
-        } else if (str[str.length - 1] === "年") { // 说明这里是选月的
-            setYy(getYy() + 1)
-            carouselAnimation('navMonth', {
-                'action': 'down',
-                'bottomUp': 304,
-                'showObj': navShowObj,
-            })
-        } else { // 说明这里是选年的
-            setYy(getYy() + 10)
-            carouselAnimation('navYear', {
-                'action': 'down',
-                'bottomUp': 304,
-                'showObj': navShowObj,
-            })
-        }
-
-    };
+  // 绑定点击事件
+  toolRight1OBJ.onclick = function () {
+    if (!throttle(500)) return;
+    let str = toolLeftOBJ.innerHTML;
+    // 这里我要判断当前是选日还是选月还是选年
+    if (str[str.length - 1] === "月") {
+      // 说明这里是选日的
+      if (getMo() > 1) {
+        setMo(getMo() - 1);
+      } else {
+        setMo(12);
+        setYy(getYy() - 1);
+      }
+      carouselAnimation("cnt", {
+        action: "up",
+        bottomUp: -276,
+        showObj: slideshowObj,
+      });
+    } else if (str[str.length - 1] === "年") {
+      // 说明这里是选月的
+      setYy(getYy() - 1);
+      carouselAnimation("navMonth", {
+        action: "up",
+        bottomUp: -304,
+        showObj: navShowObj,
+      });
+    } else {
+      // 说明这里是选年的
+      setYy(getYy() - 10);
+      carouselAnimation("navYear", {
+        action: "up",
+        bottomUp: -304,
+        showObj: navShowObj,
+      });
+    }
+  };
+  toolRight2OBJ.onclick = function () {
+    if (!throttle(500)) return;
+    let str = toolLeftOBJ.innerHTML;
+    // 这里我要判断当前是选日还是选月还是选年
+    if (str[str.length - 1] === "月") {
+      // 说明这里是选日的
+      if (getMo() < 12) {
+        setMo(getMo() + 1);
+      } else {
+        setMo(1);
+        setYy(getYy() + 1);
+      }
+      carouselAnimation("cnt", {
+        action: "down",
+        bottomUp: 276,
+        showObj: slideshowObj,
+      });
+    } else if (str[str.length - 1] === "年") {
+      // 说明这里是选月的
+      setYy(getYy() + 1);
+      carouselAnimation("navMonth", {
+        action: "down",
+        bottomUp: 304,
+        showObj: navShowObj,
+      });
+    } else {
+      // 说明这里是选年的
+      setYy(getYy() + 10);
+      carouselAnimation("navYear", {
+        action: "down",
+        bottomUp: 304,
+        showObj: navShowObj,
+      });
+    }
+  };
 }
